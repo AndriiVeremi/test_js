8a720c2a212ceeccde4d61f3ec859330e7071c42
Andrii Veremii <dashuk10@gmail.com>
2023-01-05 19:07:05 +0200

    add

---
+++ js/js_1_vars.js
@@ -0,0 +1,14 @@
+const age = 10;
+const totalPrice = 200.74;
+const userName = 'Chelsy';
+const message = 'Вітаємо!';
+const isOpen = true;
+const shouldConfirm = false; 
+
+
+// console.log('start');
+// alert('asd');
+// console.log('end');
+
+// const type = typeof totalPrice;
+// console.log(type);
---
+++ js/js_2_input.js
@@ -0,0 +1,16 @@
+// методи --> window.confirm() i window.prompt()
+
+
+// const shoudRenew = confirm('asd?');
+// console.log(shoudRenew);
+
+
+// const quantity = prompt('asdasd');
+// console.log(quantity);
+// console.log(typeof quantity);
+
+
+// let quantity = prompt('asdasd');
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
