606e132b163899037ccf045d8ea9c80de104cf41
Andrii Veremii <dashuk10@gmail.com>
2023-01-08 11:25:21 +0200

    add

--- js/module-1/11-discounts.js
+++ js/module-1/11-discounts.js
@@ -15,27 +15,104 @@
 
 
 
-let totalSpent = 1000;
-let payment = 500;
-let discount = 0;
+// let totalSpent = 1000;
+// let payment = 500;
+// let discount = 0;
 
-if (totalSpent >= 100 && totalSpent < 1000) {
-    console.log('Бронзовий партнер 2%')
-    discount = 0.02;
-} else if (totalSpent >= 1000 && totalSpent < 5000) {
-    console.log('Серебряний партнер 5%')
-    discount = 0.05;
-} else if (totalSpent >= 5000) {
-    console.log('Золотой партнер 10%')
-    discount = 0.1;
-} else {
-    console.log('Скідка 0%')
+// if (totalSpent >= 100 && totalSpent < 1000) {
+//     console.log('Бронзовий партнер 2%')
+//     discount = 0.02;
+// } else if (totalSpent >= 1000 && totalSpent < 5000) {
+//     console.log('Серебряний партнер 5%')
+//     discount = 0.05;
+// } else if (totalSpent >= 5000) {
+//     console.log('Золотой партнер 10%')
+//     discount = 0.1;
+// } else {
+//     console.log('Скідка 0%')
+// }
+
+// payment -= payment * discount;
+
+// console.log(`Замовлення оформлене на суму ${payment} знижка ${discount * 100}%`);
+
+// totalSpent += payment;
+
+// console.log(`Загальна сума потрачена в магазині  ${totalSpent}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//     // Change code below this line
+
+    
+//     let totalPrice = 0;
+//     let message;
+
+//     if (orderedQuantity === 2) {
+//         deliveryFee = 50;
+//     } else if (orderedQuantity === 4) {
+//         deliveryFee = 100;
+//     } else if (orderedQuantity === 10) {
+//         deliveryFee = 200;
+//     }
+
+//     totalPrice = (pricePerDroid * orderedQuantity) + deliveryFee;
+
+//     message = `You ordered droids worth ${totalPrice} credits.Delivery ( ${deliveryFee} credits) is included in total price.`;
+   
+//     console.log(message);
+
+//     // Change code above this line
+//     return message;
+     
+// }
+
+// makeOrderMessage(2, 100, 50);
+
+
+
+function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+    // Change code below this line
+    const totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
+    const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+
+    // Change code above this line
+    return message;
 }
 
-payment -= payment * discount;
-
-console.log(`Замовлення оформлене на суму ${payment} знижка ${discount * 100}%`);
-
-totalSpent += payment;
-
-console.log(`Загальна сума потрачена в магазині  ${totalSpent}`);
+console.log(makeOrderMessage(2, 100, 50));
+makeOrderMessage(4, 300, 100);
+console.log("You ordered droids worth 1300 credits. Delivery (100 credits) is included in total price.");
+console.log('You ordered droids worth 900 credits. Delivery (200 credits) is included in total price.');
---
+++ js/module-1/12-DOM.js
@@ -0,0 +1,27 @@
+// const btnAdd = document.querySelector('button[data-add]'); // робим змінну і добавляєм силку на кнопку за допомогою .querySelector і підставляєм тег button[data-add].
+// const resets = document.querySelector('button[data-reset]');
+// const valueInput = document.querySelector('input[data-value]'); //тут дивимся що водиться в інтупі input[data-value]
+// const outputEl = document.querySelector('.js-output');  //тут вибираю абзац за допомогою CSS селектора .js-output
+
+// let total = 0;
+
+// btnAdd.addEventListener('click', function () {   // вішаю слушителя собитий за допомогою .addEventListener і вказую тип собитія 'click' і функцію яка виконається після кліка.
+//     console.log('aaaaa');  // код який виконується після кліка.
+    
+//     const value = Number(valueInput.value); // після кліка тут переводить що було ведено в інтупі рядок в число і записує в змінну
+    
+//     console.log(value);
+    
+//     total += value; // по кліку додає все що було ведено в інтупі і зберігає в змінній total
+
+//     outputEl.textContent = total;
+       
+//     valueInput.value = '';  // чистим поле ввода після кліка
+// })
+
+// resets.addEventListener('click', function () {
+//     total = 0;
+//     outputEl.textContent = total;
+// })
+
+
