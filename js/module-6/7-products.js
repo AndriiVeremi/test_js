818e3699343802e37c8882386cea2fe4f1e24123
Andrii Veremii <dashuk10@gmail.com>
2023-02-26 14:40:01 +0200

    add

--- js/module-6/7-products.js
+++ js/module-6/7-products.js
@@ -15,84 +15,70 @@ console.log(products)
 //     onSale: true,
 // }
 
-// {
+
 // //    <article class="product" >
 // //     <h2 class="product__name">Назва:</h2>
 // //     <p class="product__descr">Опис:</p>
 // //     <p class="product__price">Ціна:</p>
-// //    </article > 
-// }
-
-const productEl = document.createElement('article');
-productEl.classList.add('product');
-
-const nameEl = document.createElement('h2');
-nameEl.textContent = `Назва: ${product.name}`;
-nameEl.classList.add('product__name');
-
-
-const descrEl = document.createElement('p');
-descrEl.textContent = `Опис: ${product.description}`;
-descrEl.classList.add('product__descr');
-
-
-const priceEl = document.createElement('p');
-priceEl.textContent = `Ціна: ${product.price}`;
-priceEl.classList.add('product__price');
-
-
-productEl.append(nameEl, descrEl, priceEl)
-
-console.log(productEl)
+// //    </article >
 
 
 
-// Пишем функцію для створення карточки продукта --------------
+// const productEl = document.createElement('article');
+// productEl.classList.add('product');
 
-{
-    //    <article class="product" >
-    //     <h2 class="product__name">Назва:</h2>
-    //     <p class="product__descr">Опис:</p>
-    //     <p class="product__price">Ціна:</p>
-    //    </article > 
-}
+// const nameEl = document.createElement('h2');
+// nameEl.textContent = `Назва: ${product.name}`;
+// nameEl.classList.add('product__name');
 
 
-const productsContainerEl = document.querySelector('.js-products')
+// const descrEl = document.createElement('p');
+// descrEl.textContent = `Опис: ${product.description}`;
+// descrEl.classList.add('product__descr');
 
 
-const makeProductCard = ({ name, description, price }) => {
-
-    const productEl = document.createElement('article');
-    productEl.classList.add('product');
-
-    const nameEl = document.createElement('h2');
-    nameEl.textContent = name;
-    nameEl.classList.add('product__name');
+// const priceEl = document.createElement('p');
+// priceEl.textContent = `Ціна: ${product.price}`;
+// priceEl.classList.add('product__price');
 
 
-    const descrEl = document.createElement('p');
-    descrEl.textContent = description;
-    descrEl.classList.add('product__descr');
+// productEl.append(nameEl, descrEl, priceEl)
+
+// console.log(productEl)
 
 
-    const priceEl = document.createElement('p');
-    priceEl.textContent = price;
-    priceEl.classList.add('product__price');
+// ===============================================================
+
+// const productsContainerEl = document.querySelector('.js-products')
 
 
-    productEl.append(nameEl, descrEl, priceEl);
+// const makeProductCard = ({ name, description, price }) => {
 
-    return productEl;
-};
+//     const productEl = document.createElement('article');
+//     productEl.classList.add('product');
 
-console.log(makeProductCard(products[1]));
+//     const nameEl = document.createElement('h2');
+//     nameEl.textContent = name;
+//     nameEl.classList.add('product__name');
+
+
+//     const descrEl = document.createElement('p');
+//     descrEl.textContent = description;
+//     descrEl.classList.add('product__descr');
+
+
+//     const priceEl = document.createElement('p');
+//     priceEl.textContent = `ЦінаЖ ${price}`;
+//     priceEl.classList.add('product__price');
+
+
+//     productEl.append(nameEl, descrEl, priceEl);
+
+//     return productEl;
+// };
+// console.log(makeProductCard(products[1]));
  
+// const elements = products.map(makeProductCard);
+// console.log(elements)
 
-// Тепер зробимо багато карточок продукта --------------
-
-
-const elements = products.map(makeProductCard);
-console.log(elements)
-
-productsContainerEl.append(...elements);
+// productsContainerEl.append(...elements);
