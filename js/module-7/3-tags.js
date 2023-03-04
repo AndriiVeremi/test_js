d9461fe702a91a75334fb4a4a8b28876813ce380
Andrii Veremii <dashuk10@gmail.com>
2023-03-04 13:03:09 +0200

    add

--- js/module-7/3-tags.js
+++ js/module-7/3-tags.js
@@ -1,56 +1,56 @@
-// делегірованіє
-// - один із многіх
-// - несколько із многіх i Set
+делегірованіє
+- один із многіх
+- несколько із многіх i Set
 
-// const tagsContainer = document.querySelector('.js-tags');
-// let selectedTag = null;
+const tagsContainer = document.querySelector('.js-tags');
+let selectedTag = null;
 
-// tagsContainer.addEventListener('click', onTagsContainerClick);
+tagsContainer.addEventListener('click', onTagsContainerClick);
 
-// function onTagsContainerClick(evt) {            //  Патерн ДаркКлоуз
-//     if (evt.target.nodeName !== 'BUTTON') {     //  для того щоб перевірити
-//         return;                                 //  чи підходить клік
-//     }
+function onTagsContainerClick(evt) {            //  Патерн ДаркКлоуз
+    if (evt.target.nodeName !== 'BUTTON') {     //  для того щоб перевірити
+        return;                                 //  чи підходить клік
+    }
 
-//     const currentActiveBtn = document.querySelector('.tags__btn--active');  // перевіряє чи е вже активна кнопка
+    const currentActiveBtn = document.querySelector('.tags__btn--active');  // перевіряє чи е вже активна кнопка
 
-//     // if (currentActiveBtn) {
-//     //     currentActiveBtn.classList.remove('.tags__btn--active');
-//     // }
-//     // або те саме
-//     currentActiveBtn?.classList.remove('tags__btn--active');
+    // if (currentActiveBtn) {
+    //     currentActiveBtn.classList.remove('.tags__btn--active');
+    // }
+    // або те саме
+    currentActiveBtn?.classList.remove('tags__btn--active');
 
-//     const nextActiveBtn = evt.target;
-//     nextActiveBtn.classList.add('tags__btn--active');
-//     selectedTag = nextActiveBtn.dataset.value;
+    const nextActiveBtn = evt.target;
+    nextActiveBtn.classList.add('tags__btn--active');
+    selectedTag = nextActiveBtn.dataset.value;
 
-//     console.log(selectedTag)
-// }
+    console.log(selectedTag)
+}
 
 
-// ==================================
+==================================
 
-// const tagsContainer = document.querySelector('.js-tags');
-// const selectedTags = new Set();
+const tagsContainer = document.querySelector('.js-tags');
+const selectedTags = new Set();
 
-// tagsContainer.addEventListener('click', onTagsContainerClick);
+tagsContainer.addEventListener('click', onTagsContainerClick);
 
-// function onTagsContainerClick(evt) {
+function onTagsContainerClick(evt) {
     
-//     if (evt.target.nodeName !== 'BUTTON') {
-//         return;
-//     }
+    if (evt.target.nodeName !== 'BUTTON') {
+        return;
+    }
 
-//     const btn = evt.target;
-//     const tag = btn.dataset.value;
-//     const isActive = btn.classList.contains('tags__btn--active');
+    const btn = evt.target;
+    const tag = btn.dataset.value;
+    const isActive = btn.classList.contains('tags__btn--active');
 
-//     if (isActive) {
-//         selectedTags.delete(tag);
-//     } else {
-//         selectedTags.add(tag);
-//     }
+    if (isActive) {
+        selectedTags.delete(tag);
+    } else {
+        selectedTags.add(tag);
+    }
 
-//     btn.classList.toggle('tags__btn--active');
+    btn.classList.toggle('tags__btn--active');
 
-// }
+}
